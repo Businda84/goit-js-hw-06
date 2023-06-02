9378d6d79467351deab2b27b297324407f58167b
Businda84 <109301774+Businda84@users.noreply.github.com>
2023-06-02 20:05:07 +0300

    HW DOM

--- js/task-04.js
+++ js/task-04.js
@@ -5,18 +5,20 @@ const deskBtn = document.querySelector("[data-action='decrement']");
 const incrBtn = document.querySelector("[data-action='increment']");
 console.dir(counterValueEl);
 
-counterValueEl.textContent = 0;
+// counterValueEl.textContent = 0;
 deskBtn.addEventListener('click', OnIncrement)
 
 incrBtn.addEventListener('click', OnDecrement)
-
+  const step = 1;
 
 function OnDecrement(params) {
+   console.log(step);
      console.log('Клик+')
-    counterValueEl.textContent += 1;
+    counterValueEl.textContent += step;
 };
 
 function OnIncrement(params) {
+ console.log(params);
      console.log('Клик-')
-    counterValueEl.textContent -= 1;
+    counterValueEl.textContent -= step;
 };
--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,14 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+
+const bodyColorEl = document.querySelector('body');
+console.log(bodyColorEl);
+const colorChange =document.querySelector('.change-color')
+console.log(colorChange);
+
+colorChange.addEventListener('click', getRandomHexColor)
+
+function getRandomHexColor() {return bodyColorEl.style.backgroundColor= `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, 0)}` 
+   ;
 }
+
